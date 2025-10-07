2bc769d6d8755b7bb09c24518754defb4b3bed1e
Saira Gulamali <sguacamole98@gmail.com>
2025-10-07 19:00:27 +0100

    add and style hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
+import HeroSection from "./components/HeroSection";
+
 function App() {
   return (
-    <>
-      <h1 className="text-7xl text-green-500 font-bold font-primary underline">
-        Hello world!
-      </h1>
-      <p className="font-secondary text-4xl">bye bye </p>
-      <button>
-        <i className="fa fa-bars"></i>
-      </button>
-    </>
+    <div className="font-secondary">
+      <HeroSection />
+    </div>
   );
 }
 
