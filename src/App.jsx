5b4ddebe25c529b62edadc0af5f2f39f6f2f5447
Saira Gulamali <sguacamole98@gmail.com>
2025-10-07 18:26:10 +0100

    configured basic installation tailwind /font awesome/ google fonts

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl text-green-500 font-bold font-primary underline">
+        Hello world!
+      </h1>
+      <p className="font-secondary text-4xl">bye bye </p>
+      <button>
+        <i className="fa fa-bars"></i>
+      </button>
+    </>
+  );
+}
+
+export default App;
